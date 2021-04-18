a3e35e176c6ea9174e2fa87f6caa8f9fe02fe3ff
Piyush Pawar <piyushpawar25@gmail.com>
2021-04-18 15:17:28 +0530

    :ambulance: Fix escape string issue for firebase key

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,7 +4,7 @@ if (!admin.apps.length) {
 	admin.initializeApp({
 		credential: admin.credential.cert({
 			clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-			privateKey: process.env.FIREBASE_PRIVATE_KEY,
+			privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
 			projectId: process.env.FIREBASE_PROJECT_ID
 		})
 	});
